7e9c4cc6a46bceedfe0a9006233261d14dcafcd5
al223ec <al223ec@student.lnu.se>
2014-12-01 17:06:22 +0100

    Rollback, har fortfarande inte fått till delete

--- WebService/Controllers/HomeController.cs
+++ WebService/Controllers/HomeController.cs
@@ -10,32 +10,9 @@ namespace WebService.Controllers
 {
     public class HomeController : Controller
     {
-        private ReservationRepository repo = new ReservationRepository(); 
         public ActionResult Index()
         {
-            return View(repo.GetAll());
-        }
-        public ActionResult Add(Reservation reservation)
-        {
-            if(ModelState.IsValid)
-            {
-                repo.Add(reservation);
-                return RedirectToAction("Index"); 
-            }
-            return View("Index");
-        }
-        public ActionResult Remove(int id)
-        {
-            repo.Remove(id);
-            return RedirectToAction("Index");
-        }
-        public ActionResult Update(Reservation reservation)
-        {
-            if(ModelState.IsValid && repo.Update(reservation))
-            {
-                return RedirectToAction("Index");
-            }
-            return View("Index");
+            return View();
         }
     }
 }
