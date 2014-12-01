083fa6ecf7f3749b41469860e8cf0f624ef9db40
al223ec <al223ec@student.lnu.se>
2014-12-01 14:15:50 +0100

    Api controller

---
+++ WebService/Controllers/WebController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebService.Models;
+using WebService.Models.Repository;
+
+namespace WebService.Controllers
+{
+    public class WebController : ApiController
+    {
+        private ReservationRepository repo = new ReservationRepository(); 
+        public IEnumerable<Reservation> GetAllReservations()
+        {
+            return repo.GetAll(); 
+        }
+        public Reservation GetReservation(int id)
+        {
+            return repo.Get(id);
+        }
+        public Reservation PostReservation(Reservation reservation)
+        {
+            return repo.Add(reservation);
+        }
+        public bool PutReservation(Reservation reservation)
+        {
+            return repo.Update(reservation); 
+        }
+        public void DeleteReservation(int id)
+        {
+            repo.Remove(id);
+        }
+    }
+}
--- WebService/Global.asax.cs
+++ WebService/Global.asax.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using WebService.App_Start;
 
 namespace WebService
 {
@@ -12,6 +14,7 @@ namespace WebService
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register); //I AM THE 2nd
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
